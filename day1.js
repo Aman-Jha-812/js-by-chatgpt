4096f60a67f92a56cc3fc8a01bb0ab532e87effc
Aman-Jha-812 <amanjhavats91023@gmail.com>
2026-08-07 16:08:11 +0530

    first commit

---
+++ day1.js
@@ -0,0 +1,24 @@
+let name = "aman";
+name  = "subham";
+
+console.log(name);
+
+const age = 22;
+console.log(age);
+
+var collage = "cutm";
+var collage = "jitm";
+console.log(collage);
+
+const student = {
+  name:"aman",
+  age : 22,
+  college : "CUTM",
+ // skills= {java,html,css,aws}
+}
+
+console.log(student.name);
+student.age= 25;
+student.city = "patna";
+delete student.college;
+console.log(student);
